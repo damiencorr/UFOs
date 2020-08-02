0a6cbe959857520b906d526559b99a0875344332
damiencorr <damien.corr@gmail.com>
2020-08-02 15:36:38 -0400

    Use the “If” Statement

--- static/js/practice_app.js
+++ static/js/practice_app.js
@@ -1,9 +1,3 @@
-// import the data from data.js
-const tableData = data;
-
-// Reference the HTML table using d3
-var tbody = d3.select("tbody");
-
 // Simple JavaScript console.log statement
 function printHello() {
     console.log("Hello there!");
